f08b26ce1ca4512d330277d60afeeb2d99d7e2c9
takkar <yurisuki915@gmail.com>
2023-01-07 12:54:47 +0900

    書籍詳細の表示

--- src/main/kotlin/com/book/manager/application/service/BookService.kt
+++ src/main/kotlin/com/book/manager/application/service/BookService.kt
@@ -3,6 +3,7 @@ package com.book.manager.application.service
 import com.book.manager.domain.model.BookWithRental
 import com.book.manager.domain.repository.BookRepository
 import org.springframework.stereotype.Service
+import java.lang.IllegalArgumentException
 
 @Service
 class BookService(
@@ -10,5 +11,9 @@ class BookService(
 ){
     fun getList(): List<BookWithRental>{
         return bookRepository.findAllWithRental()
+    }
+
+    fun getDetail(bookId: Long): BookWithRental {
+        return bookRepository.findWithRental(bookId) ?: throw IllegalArgumentException("存在しない書籍ID: $bookId")
     }
 }
