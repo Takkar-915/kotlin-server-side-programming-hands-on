2ba19cd015b42948ee153ea6ff0c57f7118233c4
takkar <yurisuki915@gmail.com>
2023-01-07 14:55:18 +0900

    書籍情報の追加

--- src/main/kotlin/com/book/manager/infrastructure/database/repository/BookRepositoryImpl.kt
+++ src/main/kotlin/com/book/manager/infrastructure/database/repository/BookRepositoryImpl.kt
@@ -4,9 +4,12 @@ import com.book.manager.domain.model.Book
 import com.book.manager.domain.model.BookWithRental
 import com.book.manager.domain.model.Rental
 import com.book.manager.domain.repository.BookRepository
+import com.book.manager.infrastructure.database.mapper.BookMapper
 import com.book.manager.infrastructure.database.mapper.custom.BookWithRentalMapper
 import com.book.manager.infrastructure.database.mapper.custom.select
 import com.book.manager.infrastructure.database.mapper.custom.selectByPrimaryKey
+import com.book.manager.infrastructure.database.mapper.insert
+import com.book.manager.infrastructure.database.record.BookRecord
 import com.book.manager.infrastructure.database.record.custom.BookWithRentalRecord
 import org.springframework.stereotype.Repository
 
@@ -14,6 +17,7 @@ import org.springframework.stereotype.Repository
 @Repository
 class BookRepositoryImpl(
     private val bookWithRentalMapper: BookWithRentalMapper,
+    private val bookMapper: BookMapper
 ) : BookRepository {
     override fun findAllWithRental(): List<BookWithRental> {
         return bookWithRentalMapper.select().map { toModel(it) }
@@ -21,6 +25,10 @@ class BookRepositoryImpl(
 
     override fun findWithRental(id: Long): BookWithRental? {
         return bookWithRentalMapper.selectByPrimaryKey(id)?.let { toModel(it) }
+    }
+
+    override fun register(book: Book) {
+        bookMapper.insert(toRecord(book))
     }
 
     private fun toModel(record: BookWithRentalRecord): BookWithRental{
@@ -32,12 +40,16 @@ class BookRepositoryImpl(
         )
         val rental = record.userId?.let {
             Rental(
-                record.id!!,
-                record.userId!!,
+                record.id,
+                record.userId,
                 record.rentalDateTime!!,
                 record.returnDateTime!!
             )
         }
         return BookWithRental(book,rental)
+    }
+
+    private fun toRecord(model: Book): BookRecord{
+        return BookRecord(model.id, model.title, model.author, model.releaseDate)
     }
 }
