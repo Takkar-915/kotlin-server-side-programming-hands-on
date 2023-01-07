5b4c46e12e76c8091d0bfe767827caef52e10c94
takkar <yurisuki915@gmail.com>
2023-01-07 15:29:31 +0900

    削除機能の追加

--- src/main/kotlin/com/book/manager/presentation/controller/AdminBookController.kt
+++ src/main/kotlin/com/book/manager/presentation/controller/AdminBookController.kt
@@ -5,6 +5,8 @@ import com.book.manager.application.service.AdminBookService
 import com.book.manager.presentation.request.RegisterBookRequest
 import com.book.manager.presentation.request.UpdateBookRequest
 import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -35,5 +37,11 @@ class AdminBookController(
     fun update(@RequestBody request: UpdateBookRequest){
         adminBookService.update(request.id, request.title, request.author, request.releaseDate)
     }
+
+    @DeleteMapping("/delete/{book_id}")
+    fun delete(@PathVariable("book_id") bookId: Long){
+        adminBookService.delete(bookId)
+    }
+
 
 }
