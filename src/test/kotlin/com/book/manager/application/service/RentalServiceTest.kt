f825af5f3bcf1a41bc974ae9f910209c6f4696a2
takkar <yurisuki915@gmail.com>
2023-01-08 22:07:51 +0900

    例外スローの検証

--- src/test/kotlin/com/book/manager/application/service/RentalServiceTest.kt
+++ src/test/kotlin/com/book/manager/application/service/RentalServiceTest.kt
@@ -8,11 +8,11 @@ import com.book.manager.domain.model.User
 import com.book.manager.domain.repository.BookRepository
 import com.book.manager.domain.repository.RentalRepository
 import com.book.manager.domain.repository.UserRepository
-import com.nhaarman.mockitokotlin2.any
-import com.nhaarman.mockitokotlin2.mock
-import com.nhaarman.mockitokotlin2.verify
-import com.nhaarman.mockitokotlin2.whenever
+import com.nhaarman.mockitokotlin2.*
+import org.junit.jupiter.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import java.lang.IllegalStateException
 import java.time.LocalDate
 import java.time.LocalDateTime
 
@@ -40,4 +40,28 @@ internal class RentalServiceTest {
         verify(bookRepository).findWithRental(bookId)
         verify(rentalRepository).endRental(bookId)
     }
+
+    @Test
+    fun `endRental when book is not rental then throw exception`(){
+        val userId = 100L
+        val bookId = 1000L
+        val user = User(userId, "test@test.com", "pass", "kotlin", RoleType.USER)
+        val book = Book(bookId, "Kotlin入門", "コトリン太郎", LocalDate.now())
+        val bookWithRental = BookWithRental(book,null)
+
+        whenever(userRepository.find(any() as Long)).thenReturn(user)
+        whenever(bookRepository.findWithRental(any())).thenReturn(bookWithRental)
+
+        val exception = Assertions.assertThrows(IllegalStateException::class.java){
+            rentalService.endRental(bookId, userId)
+        }
+
+        assertThat(exception.message).isEqualTo("未貸出の書籍です bookId:${bookId}")
+
+        verify(userRepository).find(userId)
+        verify(bookRepository).findWithRental(bookId)
+        verify(rentalRepository, times(0)).endRental(any())
+
+    }
+
 }
